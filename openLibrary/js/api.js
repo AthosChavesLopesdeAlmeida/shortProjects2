1526fcdaddd77d5aeb58025794bbcd8570de40ff
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-12-05 12:53:16 -0300

    05/11/2025

--- openLibrary/js/api.js
+++ openLibrary/js/api.js
@@ -5,7 +5,7 @@ export async function searchBook(inputVal) {
 
   const response = await fetch(url);
   const data = await response.json();
-
-  return data.docs
+          
+  return data.docs;
 };
 
--- openLibrary/js/main.js
+++ openLibrary/js/main.js
@@ -5,6 +5,9 @@ const input = document.getElementById('searchBar');
 const main = document.getElementById('section');
 const list = document.getElementById('options');
 
-input.addEventListener('change', () =>  {
-  createOption()
-})
+// Event listener que dispara enquanto o usuário digita
+input.addEventListener('input', () => {
+  createOption(input.value, list);
+});
+
+// Event listener que acontece quando a opção é clicada
